cea5818dc9d39729c1cd6631f8e7fbd851cad8b8
Pedro <pedrohddb@gmail.com>
2019-09-05 18:50:33 -0300

    Mudancas na TESTARV.C

--- T1-Modular/T1-Modular/TESTARV.C
+++ T1-Modular/T1-Modular/TESTARV.C
@@ -1,8 +1,8 @@
 /***************************************************************************
 *  $MCI Módulo de implementação: Módulo de teste específico
 *
-*  Arquivo gerado:              TESTARV.C
-*  Letras identificadoras:      TARV
+*  Arquivo gerado:              TESTMTZ.C
+*  Letras identificadoras:      TMTZ
 *
 *  Nome da base de software:    Exemplo de teste automatizado
 *  Arquivo da base de software: D:\AUTOTEST\PROJETOS\SIMPLES.BSW
@@ -15,34 +15,26 @@
 *
 *  $HA Histórico de evolução:
 *     Versão  Autor    Data     Observações
-*       3.00   avs   28/02/2003 Uniformização da interface das funções e
-*                               de todas as condições de retorno.
-*       2.00   avs   03/08/2002 Eliminação de código duplicado, reestruturação
 *       1.00   avs   15/08/2001 Início do desenvolvimento
 *
 *  $ED Descrição do módulo
-*     Este mÇodulo contém as funções específicas para o teste do
-*     módulo árvore. Ilustra como redigir um interpretador de comandos
+*     Este módulo contém as funções específicas para o teste do
+*     módulo matriz. Ilustra como redigir um interpretador de comandos
 *     de teste específicos utilizando o arcabouço de teste para C.
 *
 *  $EIU Interface com o usuário pessoa
-*     Comandos de teste específicos para testar o módulo árvore:
+*     Comandos de teste específicos para testar o módulo matriz:
 *
-*     =criar        - chama a função ARV_CriarArvore( )
-*     =insdir <Char>
-*                   - chama a função ARV_InserirDireita( <Char> )
+*     "=criar"        - chama a função MTZ_CriarMatriz( )
+*     "=inselm" <Char>
+*                   - chama a função MTZ_ObterValorCorrente( <Char> )
 *                     Obs. notação: <Char>  é o valor do parâmetro
 *                     que se encontra no comando de teste.
-*
-*     "=insesq" <Char>
-*                   - chama a função ARV_InserirEsquerda( <Char> )
-*     "=irpai"      - chama a função ARV_IrPai( )
-*     "=iresq"      - chama a função ARV_IrEsquerda( )
-*     "=irdir"      - chama a função ARV_IrDireita( )
+*     "=andar"     - chama a função MTZ_IrPai( )
 *     "=obter" <Char>
-*                   - chama a função ARV_ObterValorCorr( ) e compara
+*                   - chama a função MTZ_ObterValorCorrente( ) e compara
 *                     o valor retornado com o valor <Char>
-*     "=destroi"    - chama a função ARV_DestruirArvore( )
+*     "=destroi"    - chama a função MTZ_DestruirMatriz( )
 *
 ***************************************************************************/
 
@@ -54,16 +46,13 @@
 #include    "generico.h"
 #include    "lerparm.h"
 
-#include    "arvore.h"
+#include    "MATRIZ.h"
 
 /* Tabela dos nomes dos comandos de teste específicos */
 
-#define     CRIAR_ARV_CMD       "=criar"
-#define     INS_DIR_CMD         "=insdir"
-#define     INS_ESQ_CMD         "=insesq"
-#define     IR_PAI_CMD          "=irpai"
-#define     IR_ESQ_CMD          "=iresq"
-#define     IR_DIR_CMD          "=irdir"
+#define     CRIAR_MTZ_CMD       "=criar"
+#define     INS_ELM_CMD         "=inselm"
+#define     ANDAR_CMD           "=andar"
 #define     OBTER_VAL_CMD       "=obter"
 #define     DESTROI_CMD         "=destruir"
 
@@ -72,11 +61,11 @@
 
 /***********************************************************************
 *
-*  $FC Função: TARV Efetuar operações de teste específicas para árvore
+*  $FC Função: TMTZ Efetuar operações de teste específicas para matriz
 *
 *  $ED Descrição da função
 *     Efetua os diversos comandos de teste específicos para o módulo
-*     árvore sendo testado.
+*     matriz sendo testado.
 *
 *  $EP Parâmetros
 *     $P ComandoTeste - String contendo o comando
@@ -89,8 +78,8 @@
    TST_tpCondRet TST_EfetuarComando( char * ComandoTeste )
    {
 
-      ARV_tpCondRet CondRetObtido   = ARV_CondRetOK ;
-      ARV_tpCondRet CondRetEsperada = ARV_CondRetFaltouMemoria ;
+      MTZ_tpCondRet CondRetObtido   = MTZ_CondRetOK ;
+      MTZ_tpCondRet CondRetEsperada = MTZ_CondRetFaltouMemoria ;
                                       /* inicializa para qualquer coisa */
 
       char ValorEsperado = '?'  ;
@@ -101,9 +90,9 @@
 
       TST_tpCondRet Ret ;
 
-      /* Testar ARV Criar árvore */
+      /* Testar MTZ Criar matriz */
 
-         if ( strcmp( ComandoTeste , CRIAR_ARV_CMD ) == 0 )
+         if ( strcmp( ComandoTeste , CRIAR_MTZ_CMD ) == 0 )
          {
 
             NumLidos = LER_LerParametros( "i" ,
@@ -113,16 +102,16 @@
                return TST_CondRetParm ;
             } /* if */
 
-            CondRetObtido = ARV_CriarArvore( ) ;
+            CondRetObtido = MTZ_CriarMatriz( ) ;
 
             return TST_CompararInt( CondRetEsperada , CondRetObtido ,
-                                    "Retorno errado ao criar árvore." );
+                                    "Retorno errado ao criar matriz." );
 
-         } /* fim ativa: Testar ARV Criar árvore */
+         } /* fim ativa: Testar MTZ Criar matriz */
 
-      /* Testar ARV Adicionar filho à direita */
+      /* Testar MTZ Inserir elemento na casa corrente */
 
-         else if ( strcmp( ComandoTeste , INS_DIR_CMD ) == 0 )
+         else if ( strcmp( ComandoTeste , INS_ELM_CMD ) == 0 )
          {
 
             NumLidos = LER_LerParametros( "ci" ,
@@ -132,90 +121,14 @@
                return TST_CondRetParm ;
             } /* if */
 
-            CondRetObtido = ARV_InserirDireita( ValorDado ) ;
+            CondRetObtido = MTZ_InserirElementoNaCasaCorrente( ValorDado ) ;
 
             return TST_CompararInt( CondRetEsperada , CondRetObtido ,
-                                    "Retorno errado inserir àa direita." );
+                                    "Retorno errado inserir o elemento." );
 
-         } /* fim ativa: Testar ARV Adicionar filho à direita */
+         } /* fim ativa: Inserir elemento na casa corrente */
 
-      /* Testar ARV Adicionar filho à esquerda */
-
-         else if ( strcmp( ComandoTeste , INS_ESQ_CMD ) == 0 )
-         {
-
-            NumLidos = LER_LerParametros( "ci" ,
-                               &ValorDado , &CondRetEsperada ) ;
-            if ( NumLidos != 2 )
-            {
-               return TST_CondRetParm ;
-            } /* if */
-
-            CondRetObtido = ARV_InserirEsquerda( ValorDado ) ;
-
-            return TST_CompararInt( CondRetEsperada , CondRetObtido ,
-                                    "Retorno errado ao inserir à esquerda." );
-
-         } /* fim ativa: Testar ARV Adicionar filho à esquerda */
-
-      /* Testar ARV Ir para nó pai */
-
-         else if ( strcmp( ComandoTeste , IR_PAI_CMD ) == 0 )
-         {
-
-            NumLidos = LER_LerParametros( "i" ,
-                               &CondRetEsperada ) ;
-            if ( NumLidos != 1 )
-            {
-               return TST_CondRetParm ;
-            } /* if */
-
-            CondRetObtido = ARV_IrPai( ) ;
-
-            return TST_CompararInt( CondRetEsperada , CondRetObtido ,
-                                    "Retorno errado ao ir para pai." );
-
-         } /* fim ativa: Testar ARV Ir para nó pai */
-
-      /* Testar ARV Ir para nó à esquerda */
-
-         else if ( strcmp( ComandoTeste , IR_ESQ_CMD ) == 0 )
-         {
-
-            NumLidos = LER_LerParametros( "i" ,
-                               &CondRetEsperada ) ;
-            if ( NumLidos != 1 )
-            {
-               return TST_CondRetParm ;
-            } /* if */
-
-            CondRetObtido = ARV_IrNoEsquerda( ) ;
-
-            return TST_CompararInt( CondRetEsperada , CondRetObtido ,
-                                    "Retorno errado ao ir para esquerda." );
-
-         } /* fim ativa: Testar ARV Ir para nó à esquerda */
-
-      /* Testar ARV Ir para nó à direita */
-
-         else if ( strcmp( ComandoTeste , IR_DIR_CMD ) == 0 )
-         {
-
-            NumLidos = LER_LerParametros( "i" ,
-                               &CondRetEsperada ) ;
-            if ( NumLidos != 1 )
-            {
-               return TST_CondRetParm ;
-            } /* if */
-
-            CondRetObtido = ARV_IrNoDireita( ) ;
-
-            return TST_CompararInt( CondRetEsperada , CondRetObtido ,
-                                    "Retorno errado ao ir para direita." );
-
-         } /* fim ativa: Testar ARV Ir para nó à direita */
-
-      /* Testar ARV Obter valor corrente */
+      /* Testar MTZ Obter valor corrente */
 
          else if ( strcmp( ComandoTeste , OBTER_VAL_CMD ) == 0 )
          {
@@ -227,7 +140,7 @@
                return TST_CondRetParm ;
             } /* if */
 
-            CondRetObtido = ARV_ObterValorCorr( &ValorObtido ) ;
+            CondRetObtido = MTZ_ObterValorCorrente( &ValorObtido ) ;
 
             Ret = TST_CompararInt( CondRetEsperada , CondRetObtido ,
                                    "Retorno errado ao obter valor corrente." );
@@ -238,24 +151,43 @@
             } /* if */
 
             return TST_CompararChar( ValorEsperado , ValorObtido ,
-                                     "Conteúdo do nó está errado." ) ;
+                                     "O valor está errado." ) ;
 
-         } /* fim ativa: Testar ARV Obter valor corrente */
+         } /* fim ativa: Testar MTZ Obter valor corrente */
 
-      /* Testar ARV Destruir árvore */
+      /* Testar MTZ Andar em Direção */
+
+         else if ( strcmp( ComandoTeste , ANDAR_CMD ) == 0 )
+         {
+
+            NumLidos = LER_LerParametros( "i" ,
+                               &CondRetEsperada ) ;
+            if ( NumLidos != 1 )
+            {
+               return TST_CondRetParm ;
+            } /* if */
+
+            CondRetObtido = MTZ_AndarDirecao( ) ;
+
+            return TST_CompararInt( CondRetEsperada , CondRetObtido ,
+                                    "Retorno errado ao andar." );
+
+         } /* fim ativa: Testar MTZ Andar em Direção */
+
+      /* Testar MTZ Destruir Matriz */
 
          else if ( strcmp( ComandoTeste , DESTROI_CMD ) == 0 )
          {
 
-            ARV_DestruirArvore( ) ;
+            MTZ_DestruirMatriz( ) ;
 
             return TST_CondRetOK ;
 
-         } /* fim ativa: Testar ARV Destruir árvore */
+         } /* fim ativa: Testar MTZ Destruir Matriz */
 
       return TST_CondRetNaoConhec ;
 
-   } /* Fim função: TARV Efetuar operações de teste específicas para árvore */
+   } /* Fim função: TMTZ Efetuar operações de teste específicas para matriz */
 
 /********** Fim do módulo de implementação: Módulo de teste específico **********/
 
